495199bf340dcd71a3fb846cbae29891e7e2ba11
jfc3 <buckslice@gmail.com>
2016-04-14 22:38:26 -0700

    Update WabbitSlide UI to show final speed at game end

--- AGDCWare/Assets/Contributor Assets/WabbitSlide/Scripts/GameManager.cs
+++ AGDCWare/Assets/Contributor Assets/WabbitSlide/Scripts/GameManager.cs
@@ -8,11 +8,13 @@ namespace buckslice {
         public BunnyController bunny;
         public Image overlay;
         public Text gameTimer;
+        public Text mainMessage;
         public Text speedometer;
         public Gradient speedColor;
 
         private bool gameOver = false;
         private float maxRecordedSpeed;
+        private float speedToWin = 100.0f;
 
         void Start() {
             Time.timeScale = 0.0f;
@@ -20,14 +22,6 @@ namespace buckslice {
         }
 
         private IEnumerator instructions() {
-            RectTransform rt = gameTimer.rectTransform;
-            rt.SetAsLastSibling();
-            Vector2 amin = rt.anchorMin;
-            Vector2 amax = rt.anchorMax;
-            rt.anchorMin = Vector2.one * 0.1f;
-            rt.anchorMax = Vector2.one * 0.9f;
-            gameTimer.text = "WABBIT SLIDE\n\nMove left or right\n\nHit triple digit speed to win!";
-
             Color c = overlay.color;
             c.a = 0.5f;
             overlay.color = c;
@@ -36,13 +30,9 @@ namespace buckslice {
                 yield return null;
             }
 
-            rt.SetAsFirstSibling();
-            rt.anchorMin = amin;
-            rt.anchorMax = amax;
-
             c.a = 0.0f;
             overlay.color = c;
-
+            mainMessage.enabled = false;
             Time.timeScale = 1.0f;
         }
 
@@ -65,47 +55,56 @@ namespace buckslice {
                     float bunnySpeed = bunny.getSpeed();
                     maxRecordedSpeed = Mathf.Max(maxRecordedSpeed, bunnySpeed);
                     speedometer.text = bunnySpeed.ToString("F2");
-                    speedometer.color = speedColor.Evaluate(bunnySpeed / 100f);
+                    speedometer.color = speedColor.Evaluate(bunnySpeed / speedToWin);
+
+                    // shake the speedometer based on speed
                     Vector3 offsetMin = srt.offsetMin;
                     Vector3 offsetMax = srt.offsetMax;
-                    float growth = Mathf.Max(0.0f, Mathf.Min((bunnySpeed - 20.0f) / (100.0f - 20.0f), 1.0f));
+                    float growth = Mathf.Max(0.0f, Mathf.Min((bunnySpeed - 20.0f) / (speedToWin - 20.0f), 1.0f));
                     growth = growth * growth * 200.0f;
                     offsetMin.x = offsetMax.x = (Mathf.PerlinNoise(Time.time * 20f, 0f) - .5f) * growth;
                     offsetMin.y = offsetMax.y = (Mathf.PerlinNoise(Time.time * 13f, 0f) - .5f) * growth;
                     srt.offsetMin = offsetMin;
                     srt.offsetMax = offsetMax;
 
-                    if (maxRecordedSpeed >= 100.0f) {    // you win!!!
+                    if (maxRecordedSpeed >= speedToWin) {    // you win!!!
                         gameOver = true;
-                        speedometer.text = "You Win!";
+                        setClosingMessage("You Win!");
                         srt.offsetMin = Vector2.zero;
                         srt.offsetMax = Vector2.zero;
-                        srt.anchorMin = new Vector2(0.2f, 0.3f);
-                        srt.anchorMax = new Vector2(0.8f, 0.7f);
-
                         Camera.main.transform.parent = null;
                         Camera.main.GetComponent<AudioSource>().Play();
 
                         StartCoroutine(fadeAndExit(false, 3.5f));
                     }
-                } else {
+                } else {    // you lose :(
                     gameOver = true;
+                    setClosingMessage("Too Slow!");
                     gameTimer.text = "0.0s";
                     srt.offsetMin = Vector2.zero;
                     srt.offsetMax = Vector2.zero;
-                    srt.anchorMin = new Vector2(0.2f, 0.3f);
-                    srt.anchorMax = new Vector2(0.8f, 0.7f);
-                    speedometer.text = "Too Slow!";
+
                     StartCoroutine(fadeAndExit(false, 2.0f));
                 }
             } else {
-                // old bunny dancing code
-                float musicAngle = Time.realtimeSinceStartup * 4f * Mathf.PI / 3.031f;
-                //float r = Mathf.Sin(musicAngle) - .5f;
-                //transform.Rotate(0, r * 2f, 0, Space.World);
-                float t = (Mathf.Sin(musicAngle * 2.0f) + 1.0f) / 2.0f;
-                speedometer.color = Color.Lerp(speedColor.Evaluate(maxRecordedSpeed / 100f), Color.white, t);
+                if (maxRecordedSpeed >= speedToWin) {
+                    float t = (Mathf.Sin(Time.timeSinceLevelLoad * 10.0f) + 1.0f) / 2.0f;
+                    speedometer.color = Color.Lerp(speedColor.Evaluate(maxRecordedSpeed / speedToWin), Color.white, t);
+                    mainMessage.color = speedometer.color;
+                } else {
+                    mainMessage.color = Color.blue;
+                }
             }
+        }
+
+        private void setClosingMessage(string message) {
+            mainMessage.text = message;
+            RectTransform mmrt = mainMessage.rectTransform;
+            mmrt.offsetMin = Vector2.zero;
+            mmrt.offsetMax = Vector2.zero;
+            mmrt.anchorMin = new Vector2(0.2f, 0.3f);
+            mmrt.anchorMax = new Vector2(0.8f, 0.7f);
+            mainMessage.enabled = true;
         }
 
         private IEnumerator fadeAndExit(bool fadein, float time) {
