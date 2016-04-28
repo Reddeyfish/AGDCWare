a0e6aa632a958a6f26b034bac3906959046f8503
andresg6 <andresg6@uci.edu>
2016-04-27 18:42:22 -0700

    Initial TurtlePower Push

---
+++ AGDCWare/Assets/Contributor Assets/TurtlePower/Turtle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AndresGonzalez
+{
+    public class Turtle : MonoBehaviour
+    {
+        public float speed = 1.0f;
+
+        private int team;
+        private SpriteRenderer _SpRen;
+
+        void Start()
+        {
+            _SpRen = GetComponent<SpriteRenderer>();
+            //Assign Color
+            TeamSwap(Mathf.CeilToInt(Random.value * 3));
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            transform.position += new Vector3(0, speed * Time.fixedDeltaTime, 0);
+        }
+
+        public void TeamSwap(int newTeam)
+        {
+            if(newTeam == 1) // red
+            {
+                _SpRen.color = Color.red;
+            }
+            else if(newTeam == 2) // green
+            {
+                _SpRen.color = Color.green;
+            }
+            else if(newTeam == 3) // blue
+            {
+                _SpRen.color = Color.blue;
+            }
+            this.team = newTeam;
+        }
+    }
+}
---
+++ AGDCWare/Assets/Contributor Assets/TurtlePower/TurtleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AndresGonzalez
+{
+    public class TurtleSpawner : MonoBehaviour
+    {
+        public GameObject turtlepfab;
+        private Bounds debox;
+
+        void Start()
+        {
+            debox = GetComponent<BoxCollider2D>().bounds;
+        }
+
+        void FixedUpdate()
+        {
+            //spawnTurtle();
+        }
+
+        public void spawnTurtle()
+        {
+            GameObject turtle = Instantiate(turtlepfab) as GameObject;
+            //Get random location within collider box
+            turtle.transform.position = getRandWithinBox();
+        }
+
+        public Vector2 getRandWithinBox()
+        {
+            return (Vector2)debox.center + new Vector2(Random.Range(-debox.extents.x, debox.extents.x), Random.Range(-debox.extents.y, debox.extents.y));
+        }
+    }
+}
